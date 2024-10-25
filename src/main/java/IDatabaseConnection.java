af122214dfc35d29aab6f00e0ac599740d4dd58b
lucabruenjes <bruenjesluca@gmx.de>
2024-10-25 10:12:17 +0200

    Lucas Laptop Stand

---
+++ src/main/java/IDatabaseConnection.java
@@ -0,0 +1,10 @@
+import java.sql.SQLException;
+import java.util.Properties;
+
+public interface IDatabaseConnection {
+    IDatabaseConnection openConnection(Properties properties) throws SQLException;
+    void createAllTables() throws SQLException;
+  //  void truncateAllTables() throws SQLException;
+  //  void removeAllTables() throws SQLException;
+    void closeConnection() throws SQLException;
+}
